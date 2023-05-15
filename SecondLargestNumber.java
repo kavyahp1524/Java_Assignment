2e579922da33213700959df50d5322686769b17e
kavyahp1524 <132458848+kavyahp1524@users.noreply.github.com>
2023-05-15 12:58:40 +0530

    Arrays

---
+++ SecondLargestNumber.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class SecondLargestNumber {
+    public static void main(String[]args){
+        int[] arr= {10,20,50,40,60,90};
+        int a = 0;
+        for(int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                    a=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=a;
+                }
+            }
+        }
+        System.out.println("The smallest number in an array is:\t"+arr[1]);
+    }
+}
+
